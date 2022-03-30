91408745549ea8ffda2faa949b7efaf6b97b2e18
Minecraft_652 <47168439+Minecraft652@users.noreply.github.com>
2022-03-30 17:38:16 +0800

    Add files via upload

--- src/main/java/com/sixfivetwo/sftfinance/listener/ContainerListener.java
+++ src/main/java/com/sixfivetwo/sftfinance/listener/ContainerListener.java
@@ -19,75 +19,76 @@ import java.util.List;
 
 public class ContainerListener implements Listener {
     @EventHandler
-    public void onClick(InventoryClickEvent e) throws SQLException {
-        Player player = (Player) e.getWhoClicked();
-        InventoryView inv = player.getOpenInventory();
-        if (null == e.getClickedInventory().getHolder()) {
+    public void onClick(InventoryClickEvent e) {
+        try {
+            Player player = (Player) e.getWhoClicked();
+            InventoryView inv = player.getOpenInventory();
+            if (e.getAction().equals(InventoryAction.NOTHING)) {
+                return;
+            }
+            if (e.getClickedInventory().getHolder() instanceof InventoryHolderData) {
+                ItemStack is = e.getCurrentItem();
+                ItemStack barrier = APILibrary.getTargetItem(1);
+                if (null == is) {
+                    return;
+                }
+                if (is.equals(barrier)) {
+                    e.setCancelled(true);
+                    inv.close();
+                    return;
+                }
+                ItemStack emerald_block = APILibrary.getTargetItem(2);
+                if (is.equals(emerald_block)) {
+                    e.setCancelled(true);
+                    List<ItemStack> details = APILibrary.getItemData(inv.getTopInventory());
+                    InventoryHolderData invd = (InventoryHolderData) inv.getTopInventory().getHolder();
+                    PlayerDealData pdd = new PlayerDealData(invd.type, invd.value, invd.fromid, invd.toid, details);
+                    pdd.insertData(Main.conn);
+                    pdd.getID(Main.conn);
+                    invd.setHas(true);
+                    APILibrary.sendMessageFromToPlayer(invd, pdd.id);
+                    inv.close();
+                    return;
+                }
+                ItemStack iron_fence = APILibrary.getTargetItem(3);
+                if (is.equals(iron_fence)) {
+                    e.setCancelled(true);
+                    return;
+                }
+            }
+            if (e.getClickedInventory().getHolder() instanceof InventoryHolderEditData) {
+                ItemStack is = e.getCurrentItem();
+                ItemStack barrier = APILibrary.getTargetItem(1);
+                if (null == is) {
+                    return;
+                }
+                if (is.equals(barrier)) {
+                    e.setCancelled(true);
+                    inv.close();
+                    return;
+                }
+                ItemStack emerald_block = APILibrary.getTargetItem(2);
+                if (is.equals(emerald_block)) {
+                    e.setCancelled(true);
+                    List<ItemStack> details = APILibrary.getItemData(inv.getTopInventory());
+                    InventoryHolderEditData invd = (InventoryHolderEditData) inv.getTopInventory().getHolder();
+                    PlayerDealData pdd = new PlayerDealData(invd.id, Main.conn);
+                    pdd.setDetails(details);
+                    pdd.updateData(Main.conn);
+                    pdd.getID(Main.conn);
+                    invd.setHas(true);
+                    APILibrary.sendMessageFromToPlayerUpdate(invd, pdd.fromid, pdd.toid);
+                    inv.close();
+                    return;
+                }
+                ItemStack iron_fence = APILibrary.getTargetItem(3);
+                if (is.equals(iron_fence)) {
+                    e.setCancelled(true);
+                    return;
+                }
+            }
+        } catch (Exception ex) {
             return;
-        }
-        if (e.getAction().equals(InventoryAction.NOTHING)) {
-            return;
-        }
-        if (e.getClickedInventory().getHolder() instanceof InventoryHolderData) {
-            ItemStack is = e.getCurrentItem();
-            ItemStack barrier = APILibrary.getTargetItem(1);
-            if (null == is) {
-                return;
-            }
-            if (is.equals(barrier)) {
-                e.setCancelled(true);
-                inv.close();
-                return;
-            }
-            ItemStack emerald_block = APILibrary.getTargetItem(2);
-            if (is.equals(emerald_block)) {
-                e.setCancelled(true);
-                List<ItemStack> details = APILibrary.getItemData(inv.getTopInventory());
-                InventoryHolderData invd = (InventoryHolderData) inv.getTopInventory().getHolder();
-                PlayerDealData pdd = new PlayerDealData(invd.type, invd.value, invd.fromid, invd.toid, details);
-                pdd.insertData(Main.conn);
-                pdd.getID(Main.conn);
-                invd.setHas(true);
-                APILibrary.sendMessageFromToPlayer(invd, pdd.id);
-                inv.close();
-                return;
-            }
-            ItemStack iron_fence = APILibrary.getTargetItem(3);
-            if (is.equals(iron_fence)) {
-                e.setCancelled(true);
-                return;
-            }
-        }
-        if (e.getClickedInventory().getHolder() instanceof InventoryHolderEditData) {
-            ItemStack is = e.getCurrentItem();
-            ItemStack barrier = APILibrary.getTargetItem(1);
-            if (null == is) {
-                return;
-            }
-            if (is.equals(barrier)) {
-                e.setCancelled(true);
-                inv.close();
-                return;
-            }
-            ItemStack emerald_block = APILibrary.getTargetItem(2);
-            if (is.equals(emerald_block)) {
-                e.setCancelled(true);
-                List<ItemStack> details = APILibrary.getItemData(inv.getTopInventory());
-                InventoryHolderEditData invd = (InventoryHolderEditData) inv.getTopInventory().getHolder();
-                PlayerDealData pdd = new PlayerDealData(invd.id, Main.conn);
-                pdd.setDetails(details);
-                pdd.updateData(Main.conn);
-                pdd.getID(Main.conn);
-                invd.setHas(true);
-                APILibrary.sendMessageFromToPlayerUpdate(invd, pdd.fromid, pdd.toid);
-                inv.close();
-                return;
-            }
-            ItemStack iron_fence = APILibrary.getTargetItem(3);
-            if (is.equals(iron_fence)) {
-                e.setCancelled(true);
-                return;
-            }
         }
     }
 }
