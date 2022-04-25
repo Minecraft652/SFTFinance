5c4632bd83e7ccdff51724b5390dc2aa3e8ddffe
Minecraft_652 <47168439+Minecraft652@users.noreply.github.com>
2022-04-25 15:03:59 +0800

    Add files via upload

--- src/main/java/com/sixfivetwo/sftfinance/datalibrary/ExchangeData.java
+++ src/main/java/com/sixfivetwo/sftfinance/datalibrary/ExchangeData.java
@@ -6,10 +6,12 @@ public class ExchangeData {
     public String tokentype;
     public String price;
     public String executecommand;
+    public String executorisconsole;
 
     public ExchangeData(Map<Integer, String> FileMap) {
         tokentype = FileMap.get(1);
         price = FileMap.get(2);
         executecommand = FileMap.get(3);
+        executorisconsole = FileMap.get(4);
     }
 }
